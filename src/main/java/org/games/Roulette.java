b909e9559f3f7c73d113c6b0a64b3426d33e3612
Johan Grundling <johan.grundling@blackswan.com>
2020-05-21 22:50:07 +0200

    Added lombok. Added Wheel class to manage spins.

--- src/main/java/org/games/Roulette.java
+++ src/main/java/org/games/Roulette.java
@@ -1,7 +1,25 @@
 package org.games;
 
 public class Roulette {
-    public static void main(String [] args){
-        System.out.println("testing fat jar");
+
+    public Roulette() {
+        System.out.println("Starting roulette");
+        new Thread(this.new Wheel()).start();
     }
+
+    public static void main(String [] args){
+         new Roulette();
+    }
+
+    public class Wheel implements Runnable {
+
+        @lombok.SneakyThrows
+        public void run() {
+            while (true) {
+                Thread.sleep(1000);
+                System.out.println("test ...");
+            }
+        }
+    }
+
 }
