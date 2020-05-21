d2c334cbc77fa2ea0b51c73abd0b9b6f18d819f1
Johan Grundling <johan.grundling@blackswan.com>
2020-05-22 01:03:11 +0200

    Removed unused import

--- src/main/java/org/games/Roulette.java
+++ src/main/java/org/games/Roulette.java
@@ -10,7 +10,6 @@ import org.games.dto.PlayerQuickSearchCode;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.Scanner;
